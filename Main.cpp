99693c38c668a96398a977ea2db853f2fb41b76f
Nathan Louth <77612558+NathanLouth@users.noreply.github.com>
2021-04-05 18:23:16 +0100

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include <time.h>
 using namespace std;
 
 unsigned long long int fac(int num){
@@ -12,12 +13,16 @@ unsigned long long int fac(int num){
 
 int main(){
 
+    clock_t tStart = clock();
+    
     unsigned long long int num;
     cout << "Please enter a number and this program will find the factorial:" << endl;
     cin >> num;
 
     cout << fac(num) << endl;
 
+    printf("Time taken: %.5fs\n", (double)(clock() - tStart)/CLOCKS_PER_SEC);
+    
     return 0;
 }
 
