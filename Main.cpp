787747e663d9f9bd0c8eb1e6271adaf89c771be5
NathanLouth <nathanlouth@outlook.com>
2021-04-05 19:15:10 +0100

    Add wait at end of program

--- Main.cpp
+++ Main.cpp
@@ -16,6 +16,8 @@ int main(){
     clock_t tStart = clock();
     
     unsigned long long int num;
+    string exit;
+
     cout << "Please enter a number and this program will find the factorial:" << endl;
     cin >> num;
 
@@ -23,7 +25,7 @@ int main(){
 
     printf("Time taken: %.5fs\n", (double)(clock() - tStart)/CLOCKS_PER_SEC);
 
-    cin.ignore(); 
+    cin >> exit;
     
     return 0;
 }
